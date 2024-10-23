845ecee74b93dd5bc5f3456dd0c517cb06baa834
Rae Lancaster <taylor_rae@me.com>
2024-10-23 14:07:04 -0400

    decode() function added and implemented in menu option 2

--- lab_6_.py
+++ lab_6_.py
@@ -12,8 +12,22 @@ def encode_password(password):
     return encoded_password
 
 
+def decode_password(password):
+
+    decoded_password = ""   # setting up decoded_password
+
+    for digit in password:
+        # Shift each digit down by 3 and handle wraparound with modulo 10
+        decoded_digit = (int(digit) - 3) % 10       # Takes each inputted string digit, converts to int, subtracts 3, module 10
+        decoded_password += str(decoded_digit)      # Stores the decoded digits as a string in 'decoded password'
+
+    return decoded_password
+
 
 if __name__ == '__main__':
+
+    password = ""  # Initial value for 'password'
+    encoded = ""  # Initial value for 'encoded'
 
     while True:
         print('Menu\n'
@@ -37,7 +51,9 @@ if __name__ == '__main__':
 
         if option == 2:
          # Should just be entire def encode_password(password) statement switched to decode and -3 instead of +
-            pass
+         # Replacing (password) with (encoded) undoes previous encoding :)
+            decoded = decode_password(encoded)  # Calling decode_password function and assigning result to 'decoded'
+            print(f"The encoded password is {encoded}, and the original password is {decoded}.\n")
 
         if option == 3:
             break
